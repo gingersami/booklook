5393aa22bb94a54b71621b7fbc495489e2830b92
Dor <simhondor@gmail.com>
2018-01-01 19:45:42 +0200

    Added Support for searching by Title

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ var template = Handlebars.compile(source);
 var fetchISBN = function (searchQuery) {
     $.ajax({
         method: "GET",
-        url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:'+searchQuery,
+        url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + searchQuery,
         success: function (data) {
             // var newHTML = template(data.items[0].volumeInfo)
             // $('.book-display').append(newHTML)
             bookInfo(data);
-            
+
 
         },
         error: function (jqXHR, textStatus, errorThrown) {
@@ -19,27 +19,30 @@ var fetchISBN = function (searchQuery) {
     });
 };
 
-var fetchTitle = function (searchQuery){
+var fetchTitle = function (searchQuery) {
     $.ajax({
-        method:"GET",
-        url: 'https://www.googleapis.com/books/v1/volumes?q=intitle:'+searchQuery,
-        success: function(data){
+        method: "GET",
+        url: 'https://www.googleapis.com/books/v1/volumes?q=intitle:' + searchQuery,
+        success: function (data) {
             console.log(data);
             titleSearch(data);
         },
-          error:function(jqXHR,textStatus,errorThrown){
-              console.log(textStatus)
-          }
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus)
+        }
     })
 }
 
-$('#isbn-search').on('click', function(){
+$('#isbn-search').on('click', function () {
     var searchQuery = $('#book-search').val();
-fetchISBN(searchQuery);
+    $('.book-display').show();
+    fetchISBN(searchQuery);
 })
 
-$('#title-search').on('click',function(){
+$('#title-search').on('click', function () {
     var searchQuery = $('#book-search').val();
+    $('.book-display').show();
+
     fetchTitle(searchQuery);
 })
 
@@ -48,15 +51,25 @@ $('#title-search').on('click',function(){
 //     var title = data.items[0].volumeInfo.title
 //     $('.book-display').append(title)
 // }
-function bookInfo(data){
-    var newHTML=template(data.items[0].volumeInfo);
+function bookInfo(data) {
+    var newHTML = template(data.items[0].volumeInfo);
     $('.book-display').append(newHTML)
 
 }
 
-function titleSearch(data){
-    for(var i=0;i<9;i++){
-        var newHTML =template(data.items[i].volumeInfo);
+function titleSearch(data) {
+    for (var i = 0; i < 9; i++) {
+        var newHTML = template(data.items[i].volumeInfo);
         $('.book-display').append(newHTML);
     }
 }
+
+$('.book-display').on('click', '.show-detail', function () {
+    $('.book-display').children().hide();
+    $(this).closest('div').show();
+    $(this).closest('div').append("<a href='#' class='search-return'>return to search results</a>")
+})
+
+$('.book-display').on('click', '.search-return', function(){
+    $(this).parents().siblings().show();
+})
