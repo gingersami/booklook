98b8e86d03a15e5748a4c52215207a655b301efa
Dor <simhondor@gmail.com>
2018-01-02 10:44:47 +0200

    Added validation using parsley

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 var source = $('#books-template').html()
 var template = Handlebars.compile(source);
+$('#search-form').parsley();
 
+var fetchAuthor = function (searchQuery) {
+    $.ajax({
+        method: "GET",
+        url: 'https://www.googleapis.com/books/v1/volumes?q=inauthor:' + searchQuery,
+        beforeSend: function () {
+            $('#loader').show;
+        },
+        success: function (data) {
+            $('#loader').hide();
+            authorSearch(data)
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus);
+        }
+    })
+}
 
 var fetchISBN = function (searchQuery) {
     $.ajax({
         method: "GET",
         url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + searchQuery,
+        beforeSend: function () {
+            $('#loader').show();
+        },
+
         success: function (data) {
-            // var newHTML = template(data.items[0].volumeInfo)
-            // $('.book-display').append(newHTML)
+            $('#loader').hide();
             bookInfo(data);
 
 
@@ -23,7 +43,12 @@ var fetchTitle = function (searchQuery) {
     $.ajax({
         method: "GET",
         url: 'https://www.googleapis.com/books/v1/volumes?q=intitle:' + searchQuery,
+        beforeSend: function () {
+            $('#loader').show();
+
+        },
         success: function (data) {
+            $('#loader').hide()
             console.log(data);
             titleSearch(data);
         },
@@ -33,34 +58,35 @@ var fetchTitle = function (searchQuery) {
     })
 }
 
-$('#isbn-search').on('click', function () {
-    var searchQuery = $('#book-search').val();
-    $('.book-display').show();
-    fetchISBN(searchQuery);
-})
-
-$('#title-search').on('click', function () {
-    var searchQuery = $('#book-search').val();
-    $('.book-display').show();
-
-    fetchTitle(searchQuery);
-})
-
-// function newData(){
-//     var description = data.items[0].volumeInfo.description
-//     var title = data.items[0].volumeInfo.title
-//     $('.book-display').append(title)
-// }
 function bookInfo(data) {
+     $('#book-search').attr('data-parsley-type','number')
     var newHTML = template(data.items[0].volumeInfo);
     $('.book-display').append(newHTML)
 
 }
 
 function titleSearch(data) {
-    for (var i = 0; i < 9; i++) {
+    for (var i = 0; i < 10; i++) {
         var newHTML = template(data.items[i].volumeInfo);
         $('.book-display').append(newHTML);
+    }
+}
+
+function authorSearch(data) {
+    for (var i = 0; i < 10; i++) {
+        var newHTML = template(data.items[i].volumeInfo);
+        $('.book-display').append(newHTML);
+    }
+}
+
+function getRadioState(searchQuery) {
+
+    if ($('#isbn-radio').prop('checked')) {
+        fetchISBN(searchQuery)
+    } else if ($('#title-radio').prop('checked')) {
+        fetchTitle(searchQuery)
+    } else if ($('#author-radio').prop('checked')) {
+        fetchAuthor(searchQuery)
     }
 }
 
@@ -71,6 +97,23 @@ $('.book-display').on('click', '.show-detail', function () {
     $(this).hide();
 })
 
-$('.book-display').on('click', '.search-return', function(){
+$('.book-display').on('click', '.search-return', function () {
     $(this).parents().siblings('div, hr').show();
 })
+
+
+$('#isbn-search').on('click', function () {
+    var searchQuery = $('#book-search').val();
+    $('#search-form').parsley().validate();
+
+    $('.book-display').show();
+    getRadioState(searchQuery);
+
+})
+
+// $('#title-search').on('click', function () {
+//     var searchQuery = $('#book-search').val();
+//     $('.book-display').show();
+
+//     fetchTitle(searchQuery);
+// })
